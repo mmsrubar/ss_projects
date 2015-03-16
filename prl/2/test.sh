63f0573fe7457d91195dc4d96ad52204e893c39e
Michal Šrubař <xsruba03@stud.fit.vutbr.cz>
2015-03-16 16:45:05 +0100

    final version of the test script

--- prl/2/test.sh
+++ prl/2/test.sh
@@ -10,16 +10,12 @@ else
   # get number of process needed: log_2(num)+1
   float=`echo "l($1)/l(2)+1" | bc -l`
   cpu=`echo "($float+0.5)/1" | bc`
-
-#  echo "num: $num"
-#  echo "cpu: $cpu"
-#  echo "----------------------"
 fi;
 
 dd if=/dev/random bs=1 count=$num of=numbers &> /dev/null
 
-#mpicc -std=gnu99 -lm --prefix /usr/local/share/OpenMPI -o $PRG $PRG.c && mpirun --prefix /usr/local/share/OpenMPI -np $cpu $PRG -m
-#mpicc -std=gnu99 -lm -g -DDEBUG --prefix /usr/local/share/OpenMPI -o $PRG $PRG.c && mpirun --prefix /usr/local/share/OpenMPI -np $cpu $PRG -m
-mpicc -lm -g -std=gnu99 -DDEBUG --prefix /usr/local/share/OpenMPI -o $PRG $PRG.c && mpirun --prefix /usr/local/share/OpenMPI -np $cpu $PRG
+# use -DDEBUG for debug infomation
+# use -m to see time
+mpicc -lm -g -std=gnu99 -pedantic -Wall --prefix /usr/local/share/OpenMPI -o $PRG $PRG.c && mpirun --prefix /usr/local/share/OpenMPI -np $cpu $PRG
 
-#rm -f $PRG numbers
+rm -f $PRG numbers
