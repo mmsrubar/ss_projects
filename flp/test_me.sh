6352f3d5af14f3996fd6be350e5c0147af0a22bc
Michal Šrubař <xsruba03@stud.fit.vutbr.cz>
2015-04-09 16:49:51 +0200

    flp project looks good

---
+++ flp/test_me.sh
@@ -0,0 +1,19 @@
+PRG="plg-2-nka"
+
+make
+for in_gram in tests/*.in; do
+  infile=`basename $in_gram`
+  name=${infile%.*}
+
+  ./$PRG -2 $in_gram > tests/$name.nka.out
+
+  diff tests/$name.nka.out tests/$name.nka.ok
+  if [ "$?" -eq "0" ]; then
+    echo "> Test for $ $in_gram OK"
+  else
+    echo "Error: $in_gram failed"
+    exit 1
+  fi
+done
+
+rm -rf tests/*.nka.out
