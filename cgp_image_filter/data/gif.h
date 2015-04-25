c141d25d264602f5bd6b09a411ca10a9ae641093
Michal Šrubař <xsruba03@stud.fit.vutbr.cz>
2015-04-25 17:31:20 +0200

    bin project

---
+++ cgp_image_filter/data/gif.h
@@ -0,0 +1,6 @@
+#define POPIS "# vytvori filtr pro gif obrazek 9 vstupu (3x3 okno), 1 vystup %i i4,i3,i2,i1,i0 %o out \n"
+//Pocet vstupu a vystupu
+#define PARAM_IN 9        //pocet vstupu komb. obvodu
+#define PARAM_OUT 1       //pocet vystupu komb. obvodu
+//Pocet prvku pole
+#define DATASIZE 250*250
